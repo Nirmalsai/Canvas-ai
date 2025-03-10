816f1594cbc5e9b3a72c6d4f161dd895c8d85b1d
CH Nirmal Sai <138203410+Nirmalsai@users.noreply.github.com>
2025-03-10 17:15:54 +0530

    Update index.jsx

--- vite-project/src/pages/index.jsx
+++ vite-project/src/pages/index.jsx
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-import Logo from "@/components/logo/logo"
-import Canvas from "@/components/canvas/canvas"
-import { useRef, useState } from "react"
-import  Button  from "@/components/button";
-import  Response  from "@/components/response";
-import { uploadImage } from "../services/index";
-export default function mainPage(){
-const myRef=useRef(null);
-const [result,setResult]=useState("no response");
-const [Selection,SetSelection]=useState(false)
-const handleClickRes=async()=>{
-    
-    try{
-      
-    const response=await uploadImage(myRef.current.toDataURL());
-    if(response)
-    setResult(response.data.result)
-   
-    }
-    catch{
-      console.log("error")
-    }
-  }
-const handleClickSel=()=>{
-SetSelection(true);
-}
-return <div className="flex flex-col   w-full h-screen ">
-   <div><Logo></Logo></div> 
-    <div className="flex gap-4 w-full  h-full flex-grow">
-    <div className="w-8/12 flex-grow">     
-        <Canvas myRef={myRef} selection={Selection} setSelection={SetSelection}></Canvas>
-    </div>
-    <div className="w-4/12 h-full ">
-        <Button handleClick={handleClickRes} txt="solve" id="1"></Button>
-       
-        <Response txt={result} ></Response>
-    </div>
-    </div>
-</div>
-}
-=======
+
 import React, { useRef, useState } from "react";
 import Logo from "@/components/logo/logo";
 import Canvas from "@/components/canvas/canvas";
@@ -146,4 +105,4 @@ export default function MainPage() {
     </div>
   );
 }
->>>>>>> 796bd6a (added selection feature and pen sizes and colours)
+
